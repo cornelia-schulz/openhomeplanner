586536a6bd96dfd53357ee653173d958044be931
Cornelia Schulz <schulzc@fphcare.com>
2017-05-13 13:15:23 +1200

    Prevent VS creating JS that gets checked in. Adding TestController for integrating angular and asp.net

--- OpenHomePlanner/src/app/region-search-box-component.ts
+++ OpenHomePlanner/src/app/region-search-box-component.ts
@@ -8,13 +8,13 @@ import { IMultiSelectTexts }  from 'angular-2-dropdown-multiselect';
 import { IMultiSelectSettings } from 'angular-2-dropdown-multiselect';
 
 @Component({
-  moduleId: module.id,
-  selector: 'region-search-box',
-  templateUrl: './region-search-box.component.html',
-  styleUrls: ['region-search-box.component.css']
+    moduleId: module.id,
+    selector: 'region-search-box',
+    templateUrl: './region-search-box.component.html',
+    styleUrls: ['region-search-box.component.css']
 })
 
-export class RegionSearchBoxComponent implements OnInit{
+export class RegionSearchBoxComponent implements OnInit {
     regions: Region[];
     region: Region;
     districts: District[];
@@ -27,68 +27,68 @@ export class RegionSearchBoxComponent implements OnInit{
     suburbOptions: IMultiSelectOption[];
     districtsDisabled: boolean;
     suburbsDisabled: boolean;
-    
-
-
-  constructor(private dataService: DataService) {}
-
-  // style suburb list
-  suburbTexts: IMultiSelectTexts = {
-    checkAll: 'Select all',
-    uncheckAll: 'Deselect all',
-    checked: 'suburb selected',
-    checkedPlural: 'suburbs selected',
-    defaultTitle: 'Select a Suburb',
-    allSelected: 'All selected'
-  };
-  suburbListSettings: IMultiSelectSettings = {
-    enableSearch: true,
-    buttonClasses: 'btn btn-primary btn-block',
-    dynamicTitleMaxItems: 3,
-    displayAllSelectedText: false
-  }
 
 
 
-ngOnInit(){
-  this.getRegions();
-  this.suburbOptions = [];
-  this.districtsDisabled = true;
-  this.suburbsDisabled = true;
-}
+    constructor(private dataService: DataService) { }
 
-getRegions(){
-  this.dataService.getRegions().subscribe(regs => {
-    this.regions = regs;
-  }, (errorMsg: string) => {
-    alert(errorMsg);
-  });
-}
+    // style suburb list
+    suburbTexts: IMultiSelectTexts = {
+        checkAll: 'Select all',
+        uncheckAll: 'Deselect all',
+        checked: 'suburb selected',
+        checkedPlural: 'suburbs selected',
+        defaultTitle: 'Select a Suburb',
+        allSelected: 'All selected'
+    };
+    suburbListSettings: IMultiSelectSettings = {
+        enableSearch: true,
+        buttonClasses: 'btn btn-primary btn-block',
+        dynamicTitleMaxItems: 3,
+        displayAllSelectedText: false
+    }
 
-getDistricts(event:Event):void{
-  this.region = this.selectedRegion;
-  this.districts = this.region.district;
-  this.districtsDisabled = false;
-}
 
-getSuburbs(event:Event):void{
-  this.district = this.selectedDistrict;
-  this.suburbs = this.district.suburb;
-  console.log(this.suburbs[0].name);
 
-  for(var i = 0; i < this.suburbs.length; i++){
-    this.suburbOptions.push({id: i, name: this.suburbs[i].name})
-  }
-  console.log(this.suburbOptions[0])
-  this.suburbsDisabled = false;
-}
+    ngOnInit() {
+        this.getRegions();
+        this.suburbOptions = [];
+        this.districtsDisabled = true;
+        this.suburbsDisabled = true;
+    }
 
-getSelectedSuburbs(){
-  console.log("selected suburbs: " + this.selectedSuburbs);
-  for(var i = 0; i < this.selectedSuburbs.length; i++){
-    //
-  }
-}
+    getRegions() {
+        this.dataService.getRegions().subscribe(regs => {
+            this.regions = regs;
+        }, (errorMsg: string) => {
+            alert(errorMsg);
+        });
+    }
+
+    getDistricts(event: Event): void {
+        this.region = this.selectedRegion;
+        this.districts = this.region.district;
+        this.districtsDisabled = false;
+    }
+
+    getSuburbs(event: Event): void {
+        this.district = this.selectedDistrict;
+        this.suburbs = this.district.suburb;
+        console.log(this.suburbs[0].name);
+
+        for (var i = 0; i < this.suburbs.length; i++) {
+            this.suburbOptions.push({ id: i, name: this.suburbs[i].name })
+        }
+        console.log(this.suburbOptions[0])
+        this.suburbsDisabled = false;
+    }
+
+    getSelectedSuburbs() {
+        console.log("selected suburbs: " + this.selectedSuburbs);
+        for (var i = 0; i < this.selectedSuburbs.length; i++) {
+            //
+        }
+    }
 
 
 }
